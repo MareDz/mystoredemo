30f743fe3db48a6e5f9b1894d92301a6266f47e7
Sta te briga <asd@asd.com>
2023-04-19 23:03:57 +0200

    just code update

--- essential.config.ts
+++ essential.config.ts
@@ -1,13 +1,27 @@
 import { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
+
+const config: PlaywrightTestConfig = { 
+
+  testDir: './SmokeTests',
+  testMatch: [ 
+  'mystoredemo\tests\SmokeTests\CreateUserAndUpdateUserData.spec.ts',
+  'mystoredemo\tests\SmokeTests\DeleteAdressAndBuyAccessorie.spec.ts',
+  'mystoredemo\tests\SmokeTests\VerifyCalculationAndDeleteItems.spec.ts'
+  ],
+  fullyParallel: !true,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : 2,
+  reporter: process.env.CI ? [["junit", {
+    outputFile: "results.xml"
+  }]] : "list",
   timeout: 130000,     
-  retries: 0,         
   use: {
-    headless: true,       // change it to 'false' if you want to see the execution in browser
+    headless: process.env.CI ? true : false,       // change it to 'false' if you want to see the execution in browser
     actionTimeout: 80000,    
     video: "off",
-    screenshot: "only-on-failure"
+    screenshot: process.env.CI ? "off" : "only-on-failure"
   },
   expect: {
     timeout: 15000,
