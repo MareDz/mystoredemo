7011b6b38e69fdf9742dffef642ca99609599f93
Marko Dz <rsmarko.dz@gmail.com>
2023-04-28 22:15:47 +0200

    schedule update

--- essential.config.ts
+++ essential.config.ts
@@ -5,23 +5,19 @@ const config: PlaywrightTestConfig = {
   testDir: './tests/SmokeTests',
   testMatch: ["**.spec.ts"],  // this is only for demo 
   fullyParallel: !true,
-
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : 2,
   reporter: process.env.CI ? 'github' : [ ['json', { outputFile: 'report/results.xml' }] ],
   timeout: 130000,     
-
   use: {
     headless: process.env.CI ? true : false,       // change it to 'false' if you want to see live test process in browser
     actionTimeout: 80000,    
     video: "off",
     screenshot: process.env.CI ? "off" : "only-on-failure"
   },
-
   expect: {
     timeout: 20000,
   },
-
   projects: [
     {
       name: "Chromium",
